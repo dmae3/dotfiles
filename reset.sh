36f867549289e6cbdd03343b07fd8e49a77572c3
dai <daisuke0146@gmail.com>
2013-02-24 16:14:45 +0900

    add reset.sh

---
+++ reset.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+files=( vimrc zshrc tmux.conf dir_colors vimshrc bashrc )
+directories=( vim vimfiler vimshell )
+
+for file in ${files[@]}
+do
+    if [ -a $HOME/.${file} ]; then
+        if rm -f $HOME/.${file}; then
+            echo "removed ${file}."
+        else
+            echo "failed to remove ${file}."
+        fi
+    fi
+done
+
+for directory in ${directories[@]}
+do
+    if [ -a $HOME/.${directory} ]; then
+        if rm -rf $HOME/.${directory}; then
+            echo "removed ${directory}."
+        else
+            echo "failed to remove ${directory}."
+        fi
+    fi
+done
