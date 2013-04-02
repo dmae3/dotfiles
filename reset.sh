708f13824f6811de1ba58000a3ad046ba0e1c3f6
Maeda Daisuke <MaedaDaisuke@next-group.jp>
2013-04-03 01:16:30 +0900

    fix indent

--- reset.sh
+++ reset.sh
@@ -5,22 +5,22 @@ directories=( vim vimfiler vimshell unite )
 
 for file in ${files[@]}
 do
-    if [ -a $HOME/.${file} ]; then
-        if rm -f $HOME/.${file}; then
-            echo "removed ${file}."
-        else
-            echo "failed to remove ${file}."
-        fi
+  if [ -a $HOME/.${file} ]; then
+    if rm -f $HOME/.${file}; then
+      echo "removed ${file}."
+    else
+      echo "failed to remove ${file}."
     fi
+  fi
 done
 
 for directory in ${directories[@]}
 do
-    if [ -a $HOME/.${directory} ]; then
-        if rm -rf $HOME/.${directory}; then
-            echo "removed ${directory}."
-        else
-            echo "failed to remove ${directory}."
-        fi
+  if [ -a $HOME/.${directory} ]; then
+    if rm -rf $HOME/.${directory}; then
+      echo "removed ${directory}."
+    else
+      echo "failed to remove ${directory}."
     fi
+  fi
 done
