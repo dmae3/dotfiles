29f56d6dcaa91d0603b72c307d65cc7857cc93ed
MaedaDaisuke <maedada@current-nc-001.awsnext>
2015-04-10 19:48:38 +0900

    add install.sh

---
+++ bin/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DOTFILES_DIR=$HOME/dotfiles
+
+if type git; then
+  : #OK
+else
+  echo 'Please install git or update your path to include the git executable!'
+  exit 1;
+fi
+
+git clone https://github.com/DaiJpn/dotfiles.git $HOME/dotfiles
+sh $HOME/dotfiles/setup.sh
