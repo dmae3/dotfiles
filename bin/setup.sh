7af8ac69a1ce2e974ced09e550b982d13de7c133
maeda-daisuke <d_maeda@r.recruit.co.jp>
2017-04-21 18:20:34 +0900

    add TMUX plugin manager

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotfiles=( vim vimrc zshenv zshrc tmux.conf vimshrc bashrc gitconfig agignore vimperatorrc vimperator)
+dotfiles=( vim vimrc zshenv zshrc tmux.conf tmux vimshrc bashrc gitconfig agignore vimperatorrc vimperator)
 for file in ${dotfiles[@]}
 do
   if [ -a $HOME/.$file ]; then
